5977760b0fac2533daa1debf088360a38cb7f141
Mason <bigmasonwang@gmail.com>
2025-07-12 23:22:39 +0930

    Add SignoutButton component and integrate it into the DashboardPage for user sign-out functionality.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { SignoutButton } from "@/components/signout-button";
+
 export default function DashboardPage() {
   return (
     <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4">
       <h1 className="text-2xl font-bold">Dashboard</h1>
+      <SignoutButton />
     </main>
   );
 }
---
+++ components/signout-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { signOut } from "@/lib/auth-client";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+export function SignoutButton() {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant="outline"
+      onClick={() =>
+        signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/login"); // redirect to login page
+            },
+          },
+        })
+      }
+    >
+      Sign out
+    </Button>
+  );
+}
